df433338688bb2794918ce63cb0718ce83e53337
George Visconti Jr <visconge@amazon.com>
2023-12-30 20:44:21 -0500

    Add footer

--- src/App.js
+++ src/App.js
@@ -105,6 +105,23 @@ function App() {
               Next
           </button>}
       </div>
+
+
+    <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+        <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">The correctness is based on the chart from <a 
+              href="https://www.techopedia.com/gambling-guides/blackjack-strategy" 
+              target="_blank" 
+              rel="noopener noreferrer" 
+              class="underline">
+              this site
+            </a>
+        
+          </span>
+        
+        </div>
+    </footer>
+
     </div>
   );
 }
